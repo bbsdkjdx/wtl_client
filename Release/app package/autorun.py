cf653831aec8c98c8386e7b2b618bf6891446af3
bbsdkjdx <bbsdkjdx@163.com>
2018-02-06 22:28:26 +0800

    ping ok

--- Release/app package/autorun.py
+++ Release/app package/autorun.py
@@ -34,16 +34,55 @@ def on_html_ready():
 
 def OnTimer():
 	_id=__main__.stack['timer']
+	_on_timer(_id)
 
 def OnHotkey():
 	_id=__main__.stack['hotkey']
 	__main__.js.alert(['hotkey'],_id)
 #####################################################################################################################################
 
+names=[['','',0] for x in range(8)]
+cur_set=0
 
-pn=ping.CPing(5,lambda x:__main__.js.ifrf.show(str(x)),0,0)
+def on_ping_echo(para):
+	tm=para[1]
+	if tm>1:
+		tm=1
+	for x in names:
+		if x[1]==para[0]:
+			x[2]=tm
+
+pn=ping.CPing(8,on_ping_echo,0,0)
+
+def _on_timer(_id):
+	__main__.js.ifrf.setvalue(*[1-x[2] for x in names])
+	for x in names:
+		x[2]=1
+		if x[1]:
+			pn.send_ping(x[1])
+	
 
 def f0_ping(ip):
 	__main__.js.ifrf.show('no result')
 	pn.send_ping(ip)
 
+def f0_get_name_ip():
+	return [ '{0}  [{1}]  响应时间:{2}'.format(*x) for x in names]
+
+def f0_get_values():
+	return [x[2] for x in names]
+
+def f0_set_name_ip(x):
+	global cur_set
+	cur_set=x
+	_load_htmls('1.html')
+
+def f1_set_name_ip(nm,ip):
+	if nm!=0:
+		name=names[cur_set]
+		name[0]=nm
+		name[1]=ip
+	_load_htmls('0.html')
+
+def f1_get_name_ip():
+	return names[cur_set]
